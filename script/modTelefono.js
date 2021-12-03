3c596786a6ee2b4337c367273a662e99e384fbf7
SonoSoloMarco <mgavioli008@gmail.com>
2021-12-03 15:35:48 +0100

    Fixed Home mobile

---
+++ script/modTelefono.js
@@ -0,0 +1,7 @@
+document.getElementById("modTel").addEventListener("click", modTel);
+
+function modTel() {
+  document.getElementById("modTel").style.width = "0px";
+}
+
+console.log("funzia");
--- script/openurl.js
+++ script/openurl.js
@@ -2,3 +2,7 @@ function openUrl(url, pagina) {
     window.open(url, pagina)
 }
 
+function modTel(size) {
+    document.getElementById("myBtn").style.width = "0px";
+    document.getElementById("myBtn").style.height = "0px";
+}
